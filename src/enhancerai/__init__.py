dd0732d3316e918394aa04919aef828801177903
Lukas Mahieu <lukas.mahieu@kuleuven.be>
2024-02-27 13:33:02 +0100

    Restructure project

--- src/enhancerai/__init__.py
+++ src/enhancerai/__init__.py
@@ -1,8 +1,8 @@
 from importlib.metadata import version
 
-from . import pl, pp, tl
+from . import dataloaders, model, pl, pp, zoo
 from ._io import import_topics
 
-__all__ = ["pl", "pp", "tl", "import_topics"]
+__all__ = ["pl", "pp", "zoo", "import_topics", "model", "dataloaders"]
 
 __version__ = version("EnhancerAI")
